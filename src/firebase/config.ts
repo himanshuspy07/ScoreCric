7b053be371f91d9f39202c43e0b34a7b586b2958
Himanshu yadav <himanshuspy07@gmail.com>
2026-05-11 09:28:56 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,12 @@
-
 'use client';
 
+// Important: This configuration object is public. 
+// Security is enforced by your Firebase Security Rules.
 export const firebaseConfig = {
-  apiKey: "AIzaSyDummyKey",
-  authDomain: "project-id.firebaseapp.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyAbC123XyZ789-QweRtyUiOpAsDfGhJk",
+  authDomain: "scorecric-pro.firebaseapp.com",
+  projectId: "scorecric-pro",
+  storageBucket: "scorecric-pro.firebasestorage.app",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:a1b2c3d4e5f6g7h8i9j0"
 };
